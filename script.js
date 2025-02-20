8348fecd750a176f0ce1705c936ca97750bbc406
Patrik Persson <patrik.persson.home@gmail.com>
2025-02-20 13:26:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const closeChatbot = document.querySelector('#close-chatbot');
 
 
 // API URL and API key
-const API_KEY = 'AIzaSyARP6hleR09y_6fr8GdKV_A69kfgJV_JC8';
+const API_KEY = 'AIzaSyDAKQZvKYiX1wGwHHzZ-mj_R3g9ObsPAq0';
 const API_URL =  `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 const userData = {
